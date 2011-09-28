f7662c6ce22ef80df2d332e1067d992e9faac216
jonathon@nationalheadquarters.org <jonathon@nationalheadquarters.org>
2011-09-28 11:05:38 -0700

    update

--- examples/node/public/javascripts/paper.js
+++ examples/node/public/javascripts/paper.js
@@ -1810,7 +1810,6 @@ var Item = this.Item = Base.extend({
 			paper.project.activeLayer.addChild(this);
 		this._style = PathStyle.create(this);
 		this.setStyle(this._project.getCurrentStyle());
-		this.selectable = true;
 	},
 
 	_changed: function(flags) {
